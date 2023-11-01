a3658807732a609088e1c971c8b9014939790ea1
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-01 23:15:21 +0530

    header2.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ const Header= () => {
     return (
         <div className="flex items-center h-[80px] ">
             <div className="w-2/4">
-                <a href="#"><img src="./images/Raghunath.png" className="w-1/10 relative left-[30%]" /></a>
+                <img src="./images/Raghunath.png" className="w-1/10 relative left-[30%]" />
             </div>
             <ul className="flex items-center gap-[40px] list-none w-2/4 ">
-                <li className="hover:text-red-600 hover:underline"><a href="#">Home</a></li>
-                <li className="hover:text-red-600 hover:border-solid border-2 "><a href="#">About Us</a></li>
-                <li className="underline decoration-1"><a href="#">Process</a></li>
-                <li className="hover:text-red-600:decoration-1"><a href="#">Gallery</a></li>
-                <li className="hover:text-red-600:decoration-1"><a href="#">Contact Us</a></li>
+                <li className="hover:text-red-600 hover:underline">Home</li>
+                <li className="hover:text-red-600 hover:border-solid border-2 ">About Us</li>
+                <li className="underline decoration-1">Process</li>
+                <li className="hover:text-red-600:decoration-1">Gallery</li>
+                <li className="hover:text-red-600:decoration-1">Contact Us</li>
             </ul>
         </div>
     )
