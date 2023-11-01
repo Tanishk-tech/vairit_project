375bf845e1905a0e23e4c92446423f493890dfb3
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-01 23:21:53 +0530

    hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ const Header= () => {
             </div>
             <ul className="flex items-center gap-[40px] list-none w-2/4 ">
                 <li className="hover:text-red-600 hover:underline">Home</li>
-                <li className="hover:text-red-600 hover:border-solid border-2 ">About Us</li>
-                <li className="underline decoration-1">Process</li>
-                <li className="hover:text-red-600:decoration-1">Gallery</li>
-                <li className="hover:text-red-600:decoration-1">Contact Us</li>
+                <li className="hover:text-red-600 hover:underline ">About Us</li>
+                <li className="hover:text-red-600 hover:underline">Process</li>
+                <li className="hover:text-red-600 hover:underline">Gallery</li>
+                <li className="hover:text-red-600 hover:underline">Contact Us</li>
             </ul>
         </div>
     )
