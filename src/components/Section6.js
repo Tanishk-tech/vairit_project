3b37c15036c9f0a4f96144a50ac309d415e64501
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-03 17:28:09 +0530

    update section6

--- src/components/Section6.js
+++ src/components/Section6.js
@@ -1,7 +1,7 @@
 const Section6 = () =>{
     return (
         <div className="flex flex-col items-center bg-red px-[12%] py-[6%] relative ">
-            <img src="./images/section6_icon.png" className="absolute top-0 right-0 " />
+            <img src="./images/section6_icon.png" alt="icon" className="absolute top-0 right-0 " />
             <h1 className="text-[10rem] font-bold text-yellow">50</h1>
             <h2 className="text-3xl uppercase text-white pb-8">years of experience</h2>
             <p className="text-white text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galleof type and scrambled it to make a type specimen book</p>
