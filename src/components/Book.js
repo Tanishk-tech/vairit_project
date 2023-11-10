41f243e8ae8c5450a6df226667b4c16f81af7d9c
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-10 12:59:45 +0530

    book

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,173 +2,216 @@ import HTMLFlipBook from 'react-pageflip';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// const Book= () => {
-//     let settings={
-//         width:550,
-//         height:733,
-//         size:"stretch",
-//         minWidth:315,
-//         maxWidth:1000,
-//         minHeight:400,
-//         maxHeight:1533,
-//         maxShadowOpacity:0.5,
-//         showCover:true,
-//         mobileScrollSupport:true,
-//         className:"demo-book",
-//     } ;
+const Book= () => {
+    let settings={
+        width:100,
+        height:100,
+        size:"stretch",
+        // minWidth:315,
+        // maxWidth:1000,
+        // minHeight:400,
+        // maxHeight:1533,
+        maxShadowOpacity:0.5,
+        showCover:true,
+        mobileScrollSupport:true,
+        className:"demo-book",
+    } ;
 
-//     const PageCover = React.forwardRef((props, ref) => {
-//     return (
-//         <div className="page page-cover" ref={ref} data-density="hard">
-//             <div className="page-content">
-//                 <h2>{props.children}</h2>
-//             </div>
-//         </div>
-//     );
-//     });
+    const PageCover = React.forwardRef((props, ref) => {
+    return (
+        <div className="" ref={ref} data-density="hard">
+            <div className="">
+                {/* <img src={props.source}  alt='cover' className=''></img> */}
+                <div style={{backgroundImage:props.source}} className='w-[100%] h-[100%] z-20 fixed bg-contain'>
 
-//     const Page = React.forwardRef((props, ref) => {
-//         return (
-//             <div className="page" ref={ref}>
-//                 <div className="page-content">
-//                     <h2 className="page-header">Page header - {props.number}</h2>
-//                     <div className="page-image"></div>
-//                     <div className="page-text">{props.children}</div>
-//                     <div className="page-footer">{props.number + 1}</div>
-//                 </div>
-//             </div>
-//         );
-//     });
+                </div>
+            </div>
+        </div>
+    );
+    });
 
-//     const navigate=useNavigate();
-//     const back = () => {
-//         navigate('/');
-//     }
+    const Page = React.forwardRef((props, ref) => {
+        return (
+            <div className="" ref={ref}>
+                <div className=" ">
+                    {/* <img src={props.source} alt='pages' className='' /> */}
+                    <div style={{backgroundImage:props.source}} className='w-[100%] h-[100%] z-20 fixed bg-contain'>
 
-//     return (
-//         <div className='px-[12%] pt-[1%] flex flex-col items-center'>
-//             <button className="bg-black text-white self-end w-fit text-base font-bold uppercase text-center px-8 py-4 mb-10 hover:bg-yellow hover:text-black rounded-xl cursor-pointer max-sm:mt-3 max-sm:px-4 max-sm:py-2 max-sm:text-sm " onClick={back}>back</button>
-//             <HTMLFlipBook {...settings} className='bg-grey'>
-//                 <PageCover>BOOK TITLE</PageCover>
-//                 <Page number={1}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Page>
-//                 <Page number={2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Page>
-//                 <Page number={3}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Page>
-//                 <Page number={4}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Page>
-//                 <Page number={5}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Page>
-//                 <PageCover>THE END</PageCover>
-//             </HTMLFlipBook>
+                    </div>
+                </div>
+            </div>
+        );
+    });
+
+    const navigate=useNavigate();
+    const back = () => {
+        navigate('/');
+    }
+
+    const handlePrev=() => {
+        this.flipBook.getPageFlip().flipNext();
+    }
+
+    const handleNext=() => {
+        this.flipBook.getPageFlip().flipNext();
+    }
+
+    return (
+        <div className='px-[12%] pt-[1%] flex flex-col items-center'>
+            <div className='flex w-[100%] mb-6 items-center'>
+                <img src='./images/raghu.png' alt='logo' className=' w-[5%] h-[5%] mr-[35%] ml-[5%]'/>
+                <button className="bg-black text-white  w-fit text-base font-bold uppercase text-center px-8 py-4 ml-[45%] hover:bg-yellow hover:text-black rounded-xl cursor-pointer max-sm:mt-3 max-sm:px-4 max-sm:py-2 max-sm:text-sm " onClick={back}>back</button>
+            </div>
+            
+            <HTMLFlipBook {...settings} className='book_back mb-6'>
+                <PageCover source={"URL('./images/first.png')"} ></PageCover>
+                <Page source={"URL('./images/second.png')"}></Page>
+                <Page source={"URL('./images/third.png')"}></Page>
+                <Page source={"URL('./images/fourth.png')"}></Page>
+                <Page source={"URL('./images/5.png')"}></Page>
+                <Page source={"URL('./images/6.png')"}></Page>
+                <Page source={"URL('./images/7.png')"}></Page>
+                <Page source={"URL('./images/8.png')"}></Page>
+                <Page source={"URL('./images/9.png')"}></Page>
+                <Page source={"URL('./images/10.png')"}></Page>
+                <Page source={"URL('./images/11.png')"}></Page>
+                <Page source={"URL('./images/12.png')"}></Page>
+                <Page source={"URL('./images/13.png')"}></Page>
+                <Page source={"URL('./images/14.png')"}></Page>
+                <Page source={"URL('./images/15.png')"}></Page>
+                <Page source={"URL('./images/16.png')"}></Page>
+                <Page source={"URL('./images/17.png')"}></Page>
+                <Page source={"URL('./images/18.png')"}></Page>
+                <Page source={"URL('./images/19.png')"}></Page>
+                <Page source={"URL('./images/20.png')"}></Page>
+                <PageCover source={"URL('./images/21.png')"}></PageCover>
+            </HTMLFlipBook>
+
+            <div>
+                <button className='text-base capitalize px-4 py-2 bg-yellow mr-6' onClick={handlePrev}>prev</button>
+                <button className='text-base capitalize px-4 py-2  bg-yellow' onClick={handleNext}>next</button>
+            </div>
         
+        </div>
+    )
+}
+
+export default Book ;
+
+// const PageCover = React.forwardRef((props, ref) => {
+//     return (
+//       <div className="page page-cover bg-yellow" ref={ref} data-density="hard">
+//         <div className="page-content">
+//           <h2>{props.children}</h2>
 //         </div>
-//     )
-// }
-
-// export default Book ;
-
-const PageCover = React.forwardRef((props, ref) => {
-    return (
-      <div className="page page-cover bg-yellow" ref={ref} data-density="hard">
-        <div className="page-content">
-          <h2>{props.children}</h2>
-        </div>
-      </div>
-    );
-  });
+//       </div>
+//     );
+//   });
   
-  const Page = React.forwardRef((props, ref) => {
-    return (
-      <div className="page bg-yellow" ref={ref}>
-        <div className="page-content">
-          <h2 className="page-header">Page header - {props.number}</h2>
-          <div className="page-image"></div>
-          <div className="page-text">{props.children}</div>
-          <div className="page-footer">{props.number + 1}</div>
-        </div>
-      </div>
-    );
-  });
+//   const Page = React.forwardRef((props, ref) => {
+//     return (
+//       <div className="page bg-yellow" ref={ref}>
+//         <div className="page-content">
+//           <h2 className="page-header">Page header - {props.number}</h2>
+//           <div className="page-image"></div>
+//           <div className="page-text">{props.children}</div>
+//           <div className="page-footer">{props.number + 1}</div>
+//         </div>
+//       </div>
+//     );
+//   });
   
-  class Book extends React.Component {
-    constructor(props) {
-      super(props);
+//   class Book extends React.Component {
+//     constructor(props) {
+//       super(props);
   
-      this.state = {
-        page: 0,
-        totalPage: 0,
-      };
-    }
+//       this.state = {
+//         page: 0,
+//         totalPage: 0,
+//       };
+//     }
   
-    nextButtonClick = () => {
-        this.flipBook.pageFlip().flipNext();
-    };
+//     nextButtonClick = () => {
+//         this.flipBook.pageFlip().flipNext();
+//     };
   
-    prevButtonClick = () => {
-      this.flipBook.pageFlip().flipNext();
-    };
+//     prevButtonClick = () => {
+//       this.flipBook.pageFlip().flipNext();
+//     };
   
-    onPage = (e) => {
-      this.setState({
-        page: e.data,
-      });
-    };
+//     onPage = (e) => {
+//       this.setState({
+//         page: e.data,
+//       });
+//     };
   
-    componentDidMount() {
-      this.setState({
-        totalPage:20,
-      });
-    }
+//     componentDidMount() {
+//       this.setState({
+//         totalPage:20,
+//       });
+//     }
   
-    render() {
-      return (
-        <div>
-          <HTMLFlipBook
-            width={550}
-            height={733}
-            size="stretch"
-            minWidth={315}
-            maxWidth={1000}
-            minHeight={400}
-            maxHeight={1533}
-            maxShadowOpacity={0.5}
-            showCover={true}
-            mobileScrollSupport={true}
-            onFlip={this.onPage}
-            onChangeOrientation={this.onChangeOrientation}
-            onChangeState={this.onChangeState}
-            className="demo-book"
-            ref={(el) => (this.flipBook = el)}
-          >
+//     render() {
+//       return (
+//         <div>
+//           <HTMLFlipBook
+//             width={550}
+//             height={733}
+//             size="stretch"
+//             minWidth={315}
+//             maxWidth={1000}
+//             minHeight={400}
+//             maxHeight={1533}
+//             maxShadowOpacity={0.5}
+//             showCover={true}
+//             mobileScrollSupport={true}
+//             onFlip={this.onPage}
+//             onChangeOrientation={this.onChangeOrientation}
+//             onChangeState={this.onChangeState}
+//             className="demo-book"
+//             ref={(el) => (this.flipBook = el)}
+//             autoSize={true}
+//           >
 
-            <PageCover>BOOK TITLE</PageCover>
-            <Page number={1}>Lorem ipsum...</Page>
-            <Page number={2}>Lorem ipsum...</Page>
-            <PageCover>THE END</PageCover>
+//             <PageCover>BOOK TITLE</PageCover>
+//             <Page number={1}>Lorem ipsum...</Page>
+//             <Page number={2}>Lorem ipsum...</Page>
+//             <Page number={1}>Lorem ipsum...</Page>
+//             <Page number={2}>Lorem ipsum...</Page>
+//             <Page number={1}>Lorem ipsum...</Page>
+//             <Page number={2}>Lorem ipsum...</Page>
+//             <Page number={1}>Lorem ipsum...</Page>
+//             <Page number={2}>Lorem ipsum...</Page>
+//             <Page number={1}>Lorem ipsum...</Page>
+//             <Page number={2}>Lorem ipsum...</Page>
+//             <PageCover>THE END</PageCover>
 
-          </HTMLFlipBook>
+//           </HTMLFlipBook>
   
-          <div className="container">
-            <div>
+//           <div className="container">
+//             <div>
 
-              <button type="button" onClick={this.prevButtonClick}>
-                Previous page
-              </button>
+//               <button type="button" onClick={this.prevButtonClick}>
+//                 Previous page
+//               </button>
 
-              [<span>{this.state.page}</span> of
-               <span>{this.state.totalPage}</span>]
+//               [<span>{this.state.page}</span> of
+//                <span>{this.state.totalPage}</span>]
 
-              <button type="button" onClick={this.nextButtonClick}>
-                Next page
-              </button>
+//               <button type="button" onClick={this.nextButtonClick}>
+//                 Next page
+//               </button>
 
-            </div>
-            <div>
+//             </div>
+//             <div>
 
-              State: <i>{this.state.state}</i>, orientation: <i>{this.state.orientation}</i>
+//               State: <i>{this.state.state}</i>, orientation: <i>{this.state.orientation}</i>
 
-            </div>
-          </div>
-        </div>
-      );
-    }
-  }
+//             </div>
+//           </div>
+//         </div>
+//       );
+//     }
+//   }
 
-export default Book
+// export default Book
