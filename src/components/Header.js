1cba8947291a910d6c40c3cc22aa2067517207fa
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-16 17:07:21 +0530

    header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,35 +18,35 @@ const Header= () => {
     }
 
     const [logo , setLogo] = useState({
-        imageSrc: './images/raghuji.png',
+        imageSrc: './images/raghu.png',
         imageAlt: 'logo',
         w:"w-[20%]",
         mw:"max-sm:w-[40%]",
-        })
+        });
 
-    useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
-    }, []);
-
-    
-    const listenScrollEvent = (e) => {
-        if (window.scrollY <= 400) {
-            setLogo({
-            imageSrc: './images/raghuji.png',
+    const listenScrollEvent = async (e) => {
+        if (window.scrollY <= 800) {
+            await setLogo({
+            imageSrc: './images/raghu.png',
             imageAlt: 'logo',
             w:"w-[20%]",
             mw:"max-sm:w-[40%]",
             })
-        } else {
-            setLogo({
+        } 
+        else{
+            await setLogo({
             imageSrc: './images/raghu_desc.png',
             imageAlt: 'logo',
             w:"w-[50%]",
             mw:"max-sm:w-[80%]",
             })
         }
+        console.log("listen_fun" , logo.imageSrc);
     }
 
+    useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+    }, []);
 
     return (
         <div className="flex items-center z-20 w-[100%] h-[80px] pl-[12%] pr-[10%] bg-white fixed top-0 z-10  max-sm:px-5 max-sm:py-2 ">
