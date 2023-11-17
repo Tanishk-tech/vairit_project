c45837542067b3b48ed966446b33de76a2b01c80
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-17 11:49:01 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header= () => {
@@ -25,30 +25,29 @@ const Header= () => {
         });
 
         
-        
-        const listenScrollEvent =  (e) => {
-        if (window.scrollY <= 800) {
-            setLogo({
-            imageSrc: './images/raghu.png',
-            imageAlt: 'logo',
-            w:"w-[20%]",
-            mw:"max-sm:w-[40%]",
-            })
-        } 
-        else{
-            setLogo({
-                imageSrc: './images/raghu_desc.png',
-                imageAlt: 'logo',
-            w:"w-[50%]",
-            mw:"max-sm:w-[80%]",
-        })
-        }
-        console.log("listen_fun" , logo.imageSrc);
-    }
+    // const listenScrollEvent =  (e) => {
+    //     if (window.scrollY <= 800) {
+    //         setLogo({
+    //         imageSrc: './images/raghu.png',
+    //         imageAlt: 'logo',
+    //         w:"w-[20%]",
+    //         mw:"max-sm:w-[40%]",
+    //         })
+    //     } 
+    //     else{
+    //         setLogo({
+    //             imageSrc: './images/raghu_desc.png',
+    //             imageAlt: 'logo',
+    //         w:"w-[50%]",
+    //         mw:"max-sm:w-[80%]",
+    //     })
+    //     }
+    //     // console.log("listen_fun" , logo.imageSrc);
+    // }
     
-    useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
-    }, []);
+    // useEffect(() => {
+    // window.addEventListener('scroll', listenScrollEvent);
+    // }, []);
 
     return (
         <div className="flex items-center z-20 w-[100%] h-[80px] pl-[12%] pr-[10%] bg-white fixed top-0 z-10  max-sm:px-5 max-sm:py-2 ">
