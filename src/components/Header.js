96a195ca1364b6897dc198b7ee4122e68dec2408
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-11-20 17:31:31 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { useState ,useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState ,useRef,useEffect } from "react";
+import { Await, useNavigate } from "react-router-dom";
 
 const Header= () => {
     const [display,setDisplay] = useState("hidden");
@@ -17,37 +17,46 @@ const Header= () => {
         navigate(e.target.dataset.path);
     }
 
-    const logo={
+    // const logo={
+    //     imageSrc: './images/raghu.png',
+    //     imageAlt: 'logo',
+    //     w:"w-[20%]",
+    //     mw:"max-sm:w-[40%]",
+    //     };
+
+    const [logo , setLogo] = useState({
         imageSrc: './images/raghu.png',
         imageAlt: 'logo',
         w:"w-[20%]",
         mw:"max-sm:w-[40%]",
-        };
+        });
 
-    const scollToRef = useRef(null);
-    // const listenScrollEvent =  (e) => {
-    //     if (window.scrollY <= 800) {
-    //         setLogo({
-    //         imageSrc: './images/raghu.png',
-    //         imageAlt: 'logo',
-    //         w:"w-[20%]",
-    //         mw:"max-sm:w-[40%]",
-    //         })
-    //     } 
-    //     else{
-    //         setLogo({
-    //             imageSrc: './images/raghu_desc.png',
-    //             imageAlt: 'logo',
-    //         w:"w-[50%]",
-    //         mw:"max-sm:w-[80%]",
-    //     })
-    //     }
-    //     // console.log("listen_fun" , logo.imageSrc);
-    // }
+    // const scollToRef = useRef(null);
+    const listenScrollEvent =  (e) => {
+        if (window.scrollY <= 800) {
+            setLogo({
+            imageSrc: './images/raghu.png',
+            imageAlt: 'logohuihui',
+            w:"w-[20%]",
+            mw:"max-sm:w-[40%]",
+            })
+        } 
+        else{
+            setLogo({
+                imageSrc: './images/raghu_desc.png',
+                imageAlt: 'logo',
+            w:"w-[50%]",
+            mw:"max-sm:w-[80%]",
+        })
+        console.log("raghu desc : " , logo.imageSrc);
+        console.log("raghu desc : " , logo.imageAlt);
+        }
+        console.log("listen_fun" , logo.imageSrc);
+    }
     
-    // useEffect(() => {
-    // window.addEventListener('scroll', listenScrollEvent);
-    // }, []);
+    useEffect( () => {
+    window.addEventListener('scroll', listenScrollEvent);
+    }, []);
 
     return (
         <div className="flex items-center z-20 w-[100%] h-[80px] pl-[12%] pr-[10%] bg-white fixed top-0 z-10  max-sm:px-5 max-sm:py-2 ">
