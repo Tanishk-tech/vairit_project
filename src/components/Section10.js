78a6d7abce1ec1dc9e7d55e1bc3f3385bbdaa918
tanishk marothiya <tanishkmarothiya1212@gmail.com>
2023-12-22 13:18:28 +0530

    Update Section10.js

--- src/components/Section10.js
+++ src/components/Section10.js
@@ -13,8 +13,8 @@ const Section10 = () => {
         <div className="bg-grey pl-[12%] pr-[3%] py-[2%] flex max-sm:flex max-sm:text-center max-sm:items-center" id="s10">
             <p className="w-[50%] max-sm:hidden">Copyright © 2023 Sri Raghunath JI Overseas. All Right Reserved.</p>
             <div className="w-[50%] flex justify-end max-sm:hidden">
-                <p className="pr-10"><a className="curser-pointer hover:text-red hover:underline" onClick={termPage}>Terms & Conditions</a></p>
-                <p className=""><a className="curser-pointer hover:text-red hover:underline" onClick={privacyPage}>Privacy Policy</a></p>
+                <p className="pr-10"><span className="curser-pointer hover:text-red hover:underline" onClick={termPage}>Terms & Conditions</span></p>
+                <p className=""><span className="curser-pointer hover:text-red hover:underline" onClick={privacyPage}>Privacy Policy</span></p>
             </div>
             <p className="hidden max-sm:block">Copyright © 2023 Sri Raghunath JI Overseas. All Right Reserved. Terms & Conditions .Privacy Policy</p>
         </div>
